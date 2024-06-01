71f036c2ce4beef190a7cab3bcb89d4167a5780e
Esteban_Amor <145415808+Estebatron@users.noreply.github.com>
2024-06-01 23:43:32 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    var inches = 15994;
+    var centimeters = inches * 2.54;
+    
+    Console.WriteLine (inches + " inches is " + centimeters + " centimeters");
+  }
+}
